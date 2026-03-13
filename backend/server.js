9b60f9680295f55e6e4976b1e7209ada757625bc
Abhilash Rajora <mayankrajora316@gmail.com>
2026-03-13 13:47:59 +0530

    fix react version conflict

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const bodyParser = require("body-parser");
 
 
 
@@ -19,8 +20,8 @@ connectDB();
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+//app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 // Routes
 app.use('/api/products', require('./routes/productRoutes'));
